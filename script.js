b80b0f70a47a08b0fdf918bf4cf55113cf71bb94
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-03-19 09:45:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 //your JS code here. If required.
 function allMethods() {
   //write your code here
+	// Get all properties of the Math object
+  const mathProperties = Object.getOwnPropertyNames(Math);
+  
+  // Filter out the methods
+  const methods = mathProperties.filter(prop => typeof Math[prop] === 'function');
+  
+  // Return comma-separated list of method names
+  return methods.join(', ');
 }
 
 alert(allMethods());
